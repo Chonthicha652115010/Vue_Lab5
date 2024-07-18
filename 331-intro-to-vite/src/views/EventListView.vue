43f1cf2c181ab843121cce4dfacd485bf1859b40
NaYuuri <jennyyyy9474@gmail.com>
2024-07-18 21:03:22 +0700

    Q15

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,49 +2,27 @@
 import EventCard from '@/components/EventCard.vue';
 import EventCardSec from '@/components/EventCardSec.vue';
 import type { Event } from '@/types'
-import { ref } from 'vue'
-const events = ref<Event[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28,2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Comunity Gardening',
-    description: 'Join us as we tend to the comunity edibleplants.',
-    location: 'Flor City',
-    date: 'March 14,2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'asustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22,2022',
-    time: '11:00',
-    petsAllowed: false,
-    organizer: 'Carey Wales'
-  }
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
 
-])
+const events = ref<Event[]>(null)
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/Chonthicha652115010/set-up/events')
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.log('There was an error!', error)
+    })
+})
 </script>
 
 
 <template>
-      <h1>Events For Good</h1>
-      <!-- new element -->
+  <h1>Events For Good</h1>
+  <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <EventCardSec v-for="event in events" :key="event.id" :event="event" />
@@ -53,7 +31,7 @@ const events = ref<Event[]>([
 </template>
 
 <style scoped>
-.events{
+.events {
   display: flex;
   flex-direction: column;
   align-items: center;
