65609ffc2cf65e546e5c4f952fa8adf0c0290c6b
Chonthicha Maitham <153396723+Chonthicha652115010@users.noreply.github.com>
2024-08-09 05:52:05 +0700

    Lab4.4

--- 331-intro-to-vite/src/type.ts
+++ 331-intro-to-vite/src/type.ts
@@ -9,3 +9,22 @@ export interface Event {
     petAllowed: boolean
     organizer: string
 }
+
+export interface Student {
+    id: number
+    studentId: string
+    name: string
+    surname: string
+    gpa: number
+    image: string
+    penAmount: number
+    description: string
+}
+
+export interface MessageState {
+    message: string
+}
+
+export interface EventState {
+    event: Event | null
+}
