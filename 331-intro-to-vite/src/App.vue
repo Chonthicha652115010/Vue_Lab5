535f3e3dbddba7375287805a393f394fa4c3a97f
Chonthicha Maitham <153396723+Chonthicha652115010@users.noreply.github.com>
2024-08-12 01:01:03 +0700

    Lab5 9
    update exac-active-class

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -15,10 +15,12 @@ const pageSize = ref(2)
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view', query: { pageSize: pageSize } }">Event</RouterLink>
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-          <RouterLink :to="{ name: 'Student' }">Student</RouterLink>
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500"
+            :to="{ name: 'event-list-view', query: { pageSize: pageSize } }">Event</RouterLink>
+          
+            <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" 
+            :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
