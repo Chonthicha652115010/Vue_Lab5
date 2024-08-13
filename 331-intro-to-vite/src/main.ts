c7ba8db597015cca6f13e0284830e03c4c881398
Chonthicha Maitham <153396723+Chonthicha652115010@users.noreply.github.com>
2024-08-13 20:06:59 +0700

    fix again

--- 331-intro-to-vite/src/main.ts
+++ 331-intro-to-vite/src/main.ts
@@ -1,7 +1,6 @@
 import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -11,3 +10,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
