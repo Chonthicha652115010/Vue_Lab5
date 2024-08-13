4aba7056b234262bd27daaeffb43160ec0d6c53f
Chonthicha Maitham <153396723+Chonthicha652115010@users.noreply.github.com>
2024-08-13 19:50:14 +0700

    delete style

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -18,18 +18,6 @@ defineProps<{
 </template>
 
 <style scoped>
-/* .event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-} */
-
-/* .event-card:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px rgba(0, 0, 0, 2);
-} */
 
 .event-link {
   text-decoration: none;
